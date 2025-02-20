6601157bbec92a908f70646e2ed60b728ebdc803
Vyacheslav <91011801+Slavlned@users.noreply.github.com>
2025-02-20 23:52:26 +0500

    Cover example

---
+++ types/Cover/M3CoverHoney.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/*
+ * Кавер мёда
+ */
+public class M3CoverHoney : MonoBehaviour
+{
+    [SerializeField]
+    private M3Cover cover;
+
+    private void OnEnable()
+    {
+        cover.onCoverDamage += OnCoverDamage;
+    }
+    
+    // когда прошелся дамаг по каверу
+    private void OnCoverDamage()
+    {
+        // цели
+        cover.GetBoard().GetResult().UpdateGoal(M3ObjectType.COVER_HONEY, 1);
+        // уничтожаем
+        Destroy(cover.gameObject);
+    }
+}
