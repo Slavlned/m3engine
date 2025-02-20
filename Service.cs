c9cd20d0a85b072f4529806cf8b478549da19f6c
Vyacheslav <91011801+Slavlned@users.noreply.github.com>
2025-02-20 16:24:34 +0500

    Add files via upload

---
+++ Service.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public interface Service
+{
+    public void Initialize(Service from)
+    {
+
+    }
+}
